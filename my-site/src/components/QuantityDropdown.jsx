442ed6ec743f09e019cb8707ff1648ccf79ec0dd
iam2001Irsa <145094028+iam2001Irsa@users.noreply.github.com>
2025-09-01 01:31:54 +0400

    Quantity dropdown button completed

--- my-site/src/components/QuantityDropdown.jsx
+++ my-site/src/components/QuantityDropdown.jsx
@@ -1,51 +1,38 @@
+import { useState } from 'react';
 import './QuantityDropdown.css';
-export default function SearchBar() {
+export default function QuantityDropdown() {
+
+
+  const [quantity, setQuantity] = useState(1);
+  const [isOpen, setIsOpen] = useState(false);
+  const options = Array.from({ length: 20 }, (_, i) => i + 1);
+
 
   return (
     <>
       <div className="dropdown">
-          Quantity:
-          <input className="inputBar" type="text" defaultValue="1"/>
-          <button className='dropdown-btn'>▼</button>
+        Quantity:
+        <input className="inputBar" type='text' defaultValue="1" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
+        <button className='dropdown-btn' onClick={() => setIsOpen((prev) => !prev)}>▼</button>
 
-          {/* {isOpen && ( */}
-            <ul className="dropdown-menu">
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-            </ul>
-          {/* )} */}
+        {isOpen && (
+          <ul className="dropdown-menu">
+            {options.map((num) => (
+              <li
+                key={num}
+                onClick={() => {
+                  setQuantity(num);
+                  setIsOpen(false);
+                }}
+              >
+                {num}
+              </li>
+            ))}
+          </ul>
+        )}
+
       </div>
 
-
-
-      {/* <style>{`
-        .dropdown {
-          position: relative;
-          background-color: red;
-          width: 25vw;
-        }
-
-        .inputBar{
-          width: 4vw;
-          margin-left: 3px;
-          height: 3vw;
-          font-size: 1.8vw;
-          outline: none;
-        }
-
-        .dropdown-btn {
-          padding: 1px 2px;
-          cursor: pointer;
-          width: 3vw;
-          height: 3vw;
-          font-size: 1.8vw;
-          background-color: white;
-        }
-
-        
-      `}</style> */}
-
-    </>  
+    </>
   )
 }
